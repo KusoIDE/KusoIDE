f03556ffad9a675fd4203b171597f95ec93db3ea
Sameer Rahmani <lxsameer@gnu.org>
2010-12-22 16:17:37 +0330

    initial commit

---
+++ shitty.el
@@ -0,0 +1,25 @@
+;;   Shitty - My personal emacs IDE configuration
+;;    Copyright (C) 2010  Sameer Rahmani <lxsameer@gnu.org>
+;;
+;;    This program is free software: you can redistribute it and/or modify
+;;    it under the terms of the GNU General Public License as published by
+;;    the Free Software Foundation, either version 3 of the License, or
+;;    any later version.
+;;
+;;    This program is distributed in the hope that it will be useful,
+;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;    GNU General Public License for more details.
+;;
+;;    You should have received a copy of the GNU General Public License
+;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(defun shitty-mode (active)
+  "A peace of shit configuration that tune emacs to be easier to develop with it."
+  (interactive "n")
+  (if (= active 1)
+      ;; if 
+      (progn
+	()
+	)
+    )
