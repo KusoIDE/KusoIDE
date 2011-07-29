43cb96dbd5c9ca3492ebda0cec0cea5d57d84ec7
lxsameer <lxsameer@gnu.org>
2011-07-29 18:02:31 +0430

    installer changed

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 echo "Enter your full name (you can change it later in top level customization): "
-fullname=`read`
+read fullname
 echo "Enter your mail: "
-mail=`read`
+read mail
 echo "Where is your workspace directory: "
-workspace=`read`
+read workspace
 echo "Where do you want to put pyemacs.sh file: "
-addr=`read`
+read addr
 kusohome=`pwd`
 
 mkdir -p ~/.emacs.d
@@ -15,14 +15,16 @@ if [ -x ~/.emacs ]; then
     cp ~/.emacs ~/.emacs.backup
 fi
 
-cp conf/emacs.d/* ~/.emacs.d -rv
+cp conf/emacs.d/* ~/.emacs.d -r
 echo "Creating ~/.emacs"
-mkdir -p $addr
-cp conf/bin/pyemacs.sh $addr/ -rv
-chmod u+x $addr/pyemacs.sh
+mkdir -p "$addr"
+cp conf/bin/pyemacs.sh $addr/ -r
+chmod u+x $addr/pyemacs.sh -v
 cp conf/dotemacs ~/.emacs
-sed "s/--EMAIL--/$mail/" -i ~/.emacs
-sed "s/--FULLNAME--/$fullname/" -i ~/.emacs
-sed "s/--WORKSPACE--/$workspace/" -i ~/.emacs
-sed "s/--ADDR--/$addrcp/" -i ~/.emacs
-sed "s/--KUSOHOME--/$kusohome/" > ~/.emacs
+echo "python ./rep.py $mail $fullname $workspace $addr $kusohome"
+python ./rep.py $mail $fullname $workspace $addr $kusohome
+#sed "s/--EMAIL--/$mail/" -i ~/.emacs
+#sed "s/--FULLNAME--/$fullname/" -i ~/.emacs
+#sed "s/--WORKSPACE--/$workspace/" -i ~/.emacs
+#sed "s/--ADDR--/$addr/" -i ~/.emacs
+#sed "s/--KUSOHOME--/$kusohome/" -i ~/.emacs
