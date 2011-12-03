999af01b112d399c84e301f0bf65e89c57bb3ea4
Sameer Rahmani <lxsameer@gnu.org>
2011-12-03 14:04:57 +0330

    Installation script done

--- install.sh
+++ install.sh
@@ -1,29 +1,51 @@
 #! /bin/bash
 
-echo "Enter your full name (you can change it later in top level customization): "
-read fullname
-echo "Enter your mail: "
-read mail
-echo "Where is your workspace directory: "
-read workspace
-echo "Where do you want to put pyemacs.sh file: "
-read addr
-kusohome=`pwd`
+VERSION="0.9.0"
 
+# Gathering informations
+echo -e "\n\tKuso IDE v$VERSION copyright 2010-2011 Sameer Rahmani <lxsameer@gnu.org>\n\n"
+echo "Enter requested informations. You can change it later in top"
+echo -e "level customization.\n\n"
+read -p "Enter your full name: " fullname
+read -p "Enter your email address: " mail
+read -p "Where is your workspace directory[~/src/]: " workspace
+read -p "Where do you want to put pyemacs.sh file[~/bin/]: " addr
+echo -e "\n"
+
+# Validating informations
+if [ "$workspace" == "" ]
+then
+    workspace="~/src/"
+fi
+
+if [ "$addr" == "" ]
+then
+    addr="~/bin/"
+fi
+
+# Installing stage1
+kusohome=`pwd`
 mkdir -p ~/.emacs.d
-if [ -x ~/.emacs ]; then
+if [ -e ~/.emacs ]; then
+    echo "Backing up exists .emacs file . . ."
     cp ~/.emacs ~/.emacs.backup
 fi
 
+echo "Copying files . . . "
 cp conf/emacs.d/* ~/.emacs.d -r
-echo "Creating ~/.emacs"
-mkdir -p "$addr"
+mkdir -p $addr
 cp conf/bin/pyemacs.sh $addr/ -r
-chmod u+x $addr/pyemacs.sh -v
-cp conf/dotemacs ~/.emacs
-echo "python ./rep.py $mail $fullname $workspace $addr $kusohome"
-sed "s/--EMAIL--/$mail/" -i ~/.emacs
-sed "s/--FULLNAME--/$fullname/" -i ~/.emacs
-sed "s/--WORKSPACE--/$workspace/" -i ~/.emacs
-sed "s/--ADDR--/$addr/" -i ~/.emacs
-sed "s/--KUSOHOME--/$kusohome/" -i ~/.emacs
+chmod u+x $addr/pyemacs.sh
+
+echo "Creating ~/.emacs"
+dotemacs=~/.AAA
+cp conf/dotemacs $dotemacs
+v="s/--EMAIL--/$fullname/"
+sed "s/--EMAIL--/$mail/mg" -i $dotemacs
+sed "s/--FULLNAME--/$fullname/mg" -i $dotemacs
+sed "s,--WORKSPACE--,$workspace,mg" -i $dotemacs
+sed "s,--ADDR--,$addr,mg" -i $dotemacs
+sed "s,--KUSOHOME--,$kusohome,mg" -i $dotemacs
+
+echo "Installation finished."
+echo "Restart the GNU/Emacs and make sure that all the requirements met."
