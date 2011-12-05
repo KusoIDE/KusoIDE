5743613f21e500e127080a28afa45d4b901dc117
Sameer Rahmani <lxsameer@gnu.org>
2011-12-05 23:15:26 +0330

    A Note added to installer

--- install.sh
+++ install.sh
@@ -77,3 +77,5 @@ echo
 echo "export PATH=\$PATH:$repo"
 echo -e "\nInstallation finished."
 echo "Restart the GNU/Emacs and make sure that all the requirements met."
+echo
+printf "\033[01;33mImportant Note:\033[00m Do NOT remove the Kuso IDE source."
