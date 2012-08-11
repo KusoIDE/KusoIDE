30e30ddc1e828fef8bdbb5b885e28a302be626f4
Sameer Rahmani <lxsameer@gnu.org>
2012-08-11 20:00:37 +0430

    session management added (save failed)

--- src/lib/menu.el
+++ src/lib/menu.el
@@ -35,18 +35,25 @@
   (define-key global-map [menu-bar file new-proj elproj elgeneric] '("Generic project" . elgeneric))
 )
 
-
+(defun save-session () "wrap the desktop-save"
+  (let (path)
+    (desktop-save-in-desktop-dir)
+    )
+)
 (defun menu/init-menu () "Initializing Kuso IDE menu"
   ;; New Project Menu
   (interactive)
   (define-key-after global-map [menu-bar file new-proj] (cons "New Project" (make-sparse-keymap "new project")) 'new-file)
+  (define-key-after global-map [menu-bar file load-session] '("Load Session" . desktop-change-dir) 'new-proj)
+  (define-key-after global-map [menu-bar file save-session] '("Save Session" . save-session) 'load-session)
+
   ;; New Project sub menus
-;;  (define-key-after global-map [menu-bar file new-proj cproj] (cons "C/C++" (make-sparse-keymap "c-cpp-proj")))
-;;  (define-key-after global-map [menu-bar file new-proj pyproj] (cons "Python" (make-sparse-keymap "python-proj")) 'cproj)
-;;  (define-key-after global-map [menu-bar file new-proj elproj] (cons "Elisp" (make-sparse-keymap "el-proj")) 'phpproj)
-;;  (define-key-after global-map [menu-bar file new-proj jproj] (cons "Java" (make-sparse-keymap "j-proj")) 'elproj)
-;;  (define-key-after global-map [menu-bar file new-proj jsproj] (cons "JS" (make-sparse-keymap "js-proj")) 'jproj)
-;;  (define-key-after global-map [menu-bar file new-proj phpproj] (cons "PHP" (make-sparse-keymap "php-proj")) 'pyproj)
+  (define-key-after global-map [menu-bar file new-proj cproj] (cons "C/C++" (make-sparse-keymap "c-cpp-proj")))
+  (define-key-after global-map [menu-bar file new-proj pyproj] (cons "Python" (make-sparse-keymap "python-proj")) 'cproj)
+  (define-key-after global-map [menu-bar file new-proj elproj] (cons "Elisp" (make-sparse-keymap "el-proj")) 'phpproj)
+  (define-key-after global-map [menu-bar file new-proj jproj] (cons "Java" (make-sparse-keymap "j-proj")) 'elproj)
+  (define-key-after global-map [menu-bar file new-proj jsproj] (cons "JS" (make-sparse-keymap "js-proj")) 'jproj)
+  (define-key-after global-map [menu-bar file new-proj phpproj] (cons "PHP" (make-sparse-keymap "php-proj")) 'pyproj)
   (define-key-after global-map [menu-bar help-menu about-kuso] '("About KusoIDE" . about-kuso-f) 'about-emacs)
   (message "Menus Initinalized")
 )
